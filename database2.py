25480d6ae8d61440a7fe5b255be8f2d93ab7cd8b
agam2468el10-netizen <agam2468el10@gmail.com>
2026-08-18 14:13:32 +0300

    Add files via upload

--- database2.py
+++ database2.py
@@ -3,9 +3,8 @@ import os
 
 
 file_name = 'data.csv'
-
+data = {}
 def creat_csv():
-    data = {}
     df = pd.DataFrame(data)
     if not os.path.exists(file_name):
         df.to_csv(file_name)
@@ -14,12 +13,18 @@ def creat_csv():
 
 creat_csv()
 
-def save_new_data(num_key, matrix,matrix_grass,solider_loc):
-    data = creat_csv()
+def save_new_data(num_key, matrix,matrix_grass,solider_loc, data):
     data[num_key] = [matrix,matrix_grass,solider_loc]
     df = pd.DataFrame(data)
     df.to_csv(file_name)
-    return data
+
+def return_data(num_key):
+    df = pd.read_csv(file_name)
+    rows = df.iloc[0:3,num_key]
+    new_matrix = rows[0]
+    new_grass = rows[1]
+    new_solider_loc =rows[2]
+    return new_matrix,new_grass,new_solider_loc
 
 
 
