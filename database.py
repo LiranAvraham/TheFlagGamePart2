afe0e0e48f6809e2ea1e509da6aeffa1cb945d3c
LiranAvraham <150067511+LiranAvraham@users.noreply.github.com>
2026-08-18 16:45:24 +0300

    Add files via upload

--- database.py
+++ database.py
@@ -1,37 +1,46 @@
 import pandas as pd
 import os
-import csv
-import json
-import pygame
 
-database_data = ["soldier_row", "soldier_col", "matrix", "grass_matrix"]
-file_name = "save_game_progress.csv"
-# database = pd.read_csv(file_name)
+from newfolder.screen import draw_game
 
-NUMBER_KEYS = {
-    pygame.K_1: 1,
-    pygame.K_2: 2,
-    pygame.K_3: 3,
-    pygame.K_4: 4,
-    pygame.K_5: 5,
-    pygame.K_6: 6,
-    pygame.K_7: 7,
-    pygame.K_8: 8,
-    pygame.K_9: 9
-}
+# import json
 
-def save_game(key, soldier_row, soldier_col, matrix, grass_matrix):
 
-    game_data = {
-        "soldier_row": soldier_row,
-        "soldier_col": soldier_col,
-        "matrix": matrix,
-        "grass_matrix": grass_matrix
-    }
 
-def init_db():
+file_name = 'save_game_progress.csv'
+data = {}
+
+def creat_csv():
+    data = {}
+    df = pd.DataFrame(data)
     if not os.path.exists(file_name):
-        database = pd.DataFrame(columns=["soldier_row", "soldier_col", "matrix", "grass_matrix"])
-        database.to_csv(database, index = False)
+        df.to_csv(file_name)
 
+creat_csv()
 
+def save_new_data(num_key, matrix, matrix_grass, solider_loc, data):
+    data[num_key] = [matrix, matrix_grass, solider_loc]
+    df = pd.DataFrame(data)
+    df.to_csv(file_name)
+
+    return data
+
+def return_data(num_key):
+    df = pd.read_csv(file_name)
+    rows = df.iloc[0:3,num_key]
+    new_matrix = rows[0]
+    new_grass = rows[1]
+    new_solider_loc = rows[2]
+
+    return new_matrix, new_grass, new_solider_loc
+
+def load_game(key):
+    df = pd.read_csv(file_name)
+    saved_game = save_new_data().iloc[0]
+
+    soldier_row = int(saved_game["soldier_row"])
+    soldier_col = int(saved_game["soldier_col"])
+    matrix = saved_game["matrix"]
+    grass_matrix = saved_game["grass_matrix"]
+
+    return draw_game(soldier_row, soldier_col, matrix, grass_matrix)
